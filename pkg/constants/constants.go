6cecd64902b3d634f01c76500c125731a271a541
harekrishnarai <786hkr@gmail.com>
2026-03-27 11:27:44 +0530

    chore: bump version to v1.1.0

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -22,7 +22,7 @@ import "os"
 const (
 	// Version information
 	AppName    = "flowlyt"
-	AppVersion = "1.0.11"
+	AppVersion = "1.1.0"
 	AppUsage   = "Multi-Platform CI/CD Security Analyzer"
 
 	// Default configuration values
